48a8c730ab3ddb29011e20d4fffb541a7a2b888b
Elias José Fadel Vieira <fadel.elias@estudante.ufjf.br>
2026-01-27 21:07:47 -0300

    fix gh-pages

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ufjf-dcc207-2025-3-a-sem2-amigos-do-sao-pedro-2/',
+  base: '/ufjf-dcc207-2025-3-a-sem03-amigos-do-sao-pedro-2/',
   plugins: [react()],
 })
